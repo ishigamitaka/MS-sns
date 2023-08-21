3183307050286a094ddc6d2d797eab824ed3be3b
ishigamitaka <nantoka2580147@gmail.com>
2023-08-21 09:18:12 +0000

    before_action

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,15 @@
 class Public::HomesController < ApplicationController
+  before_action :move_to_signed_in, except: [:top]
   def top
     @posts = Post.order(created_at: :desc)
   end 
 end
+
+
+private
+  def move_to_signed_in
+    unless user_signed_in?
+      #サインインしていないユーザーはログインページが表示される
+      redirect_to new_user_session_path
+    end
+  end
